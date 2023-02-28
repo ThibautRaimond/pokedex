a89b317390684064c767777dd7f89f5c5bc8f3e4
Thibaut <raimond.thibaut@gmail.com>
2023-02-28 13:26:57 +0100

    Pokemons V1 in french

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,16 +1,16 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-import "../styles/resetCss.css"
-import "../styles/homePage.css"
+import "../styles/resetCss.css";
+import "../styles/homePage.css";
 
 const HomePage = () => {
 const [pokemonList, setPokemonList] = useState([]);
-const [searchTerm, setSearchTerm] = useState(""); // Nouvel état pour stocker le terme de recherche
+const [searchTerm, setSearchTerm] = useState("");
 
 useEffect(() => {
 	axios
-		.get("https://pokeapi.co/api/v2/pokemon?limit=151")
+		.get("https://pokeapi.co/api/v2/pokemon-species?limit=151")
 		.then((response) => {
 			setPokemonList(response.data.results);
 		})
@@ -23,45 +23,41 @@ const [pokemonDetails, setPokemonDetails] = useState([]);
 
 useEffect(() => {
 	const fetchData = async () => {
-		const promises = pokemonList.map((pokemon, index) =>
-			axios.get(`https://pokeapi.co/api/v2/pokemon/${index + 1}`)
+		const promises = pokemonList.map((pokemon) =>
+			axios.get(pokemon.url)
 		);
 		const responses = await Promise.all(promises);
 		const details = responses.map((response) => {
-			const { name, sprites, types } = response.data;
-			const classes = types.map((type) => type.type.name);
-			return { name, image: sprites.front_default, classes };
+			const name = response.data.names.find((n) => n.language.name === "fr").name;
+			const image = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${response.data.id}.png`;
+			const classes = response.data.genera
+				.filter((g) => g.language.name === "fr")
+				.map((g) => g.genus);
+			return { name, image, classes };
 		});
 		setPokemonDetails(details);
 	};
 	fetchData();
 }, [pokemonList]);
 
-// Nouvelle fonction pour filtrer les Pokémons en fonction du terme de recherche
 const filteredPokemonDetails = pokemonDetails.filter((pokemon) => {
 	return pokemon.name.toLowerCase().includes(searchTerm.toLowerCase());
 });
 
 return (
 	<main>
-		<h1>Pokémon List</h1>
+		<h1>Liste des Pokémon</h1>
 		<input
 			type="text"
-			placeholder="Search by name"
+			placeholder="Rechercher par nom"
 			value={searchTerm}
 			onChange={(event) => setSearchTerm(event.target.value)}
 		/>
 		<ul className="pokemons">
 			{filteredPokemonDetails.map((pokemon, index) => (
-				<li key={index} className="pokemon">
-				<p> {pokemon.name}</p>
-				<img src={pokemon.image} alt={pokemon.name} />
-					<p>Class(es): </p>
-					<ul>
-						{pokemon.classes.map((classe, index) => (
-							<li key={index}>{classe}</li>
-						))}
-					</ul>
+				<li className="pokemon" key={index}>
+					<p>{pokemon.name}</p>
+						<img src={pokemon.image} alt={pokemon.name} />
 				</li>
 			))}
 		</ul>
