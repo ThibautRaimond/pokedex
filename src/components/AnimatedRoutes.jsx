1095e61305236209b69674e3e4e5afeeed1b0968
Thibaut <raimond.thibaut@gmail.com>
2023-03-02 19:01:41 +0100

    suppression de framer-motion

--- src/components/AnimatedRoutes.jsx
+++ src/components/AnimatedRoutes.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Routes, Route, useLocation } from "react-router-dom";
-import { AnimatePresence } from "framer-motion";
 
 import HomePage from "../pages/HomePage";
 import Pokedex from "../pages/Pokedex";
@@ -8,12 +7,10 @@ import Pokedex from "../pages/Pokedex";
 const AnimatedRoutes = () => {
 	const location = useLocation();
 	return (
-		<AnimatePresence>
 			<Routes location={location.pathname} key={location.pathname}>
 				<Route path="/pokedex" element={<HomePage />} />
 				<Route path="/pokedex/pokemon/:id" element={<Pokedex />} />
 			</Routes>
-		</AnimatePresence>
 	);
 };
 
