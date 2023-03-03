ab0e702a40096fb6033c29cd7bf1efc6dbdc8e66
Thibaut <raimond.thibaut@gmail.com>
2023-03-03 22:48:00 +0100

    dark mode done

--- src/components/ModeToggle.jsx
+++ src/components/ModeToggle.jsx
@@ -1,14 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import "../styles/components/modeToggle.css";
 
 const ModeToggle = ({ onToggle }) => {
+	const [isDarkMode, setIsDarkMode] = useState(false);
+
 	const handleToggle = () => {
+		setIsDarkMode(!isDarkMode);
 		onToggle();
 	};
 
 	return (
 		<div className="toggle">
-			<input type="checkbox" id="toggle-switch" onChange={handleToggle} />
-			<label htmlFor="toggle-switch">Mode nuit</label>
+			<input
+				type="checkbox"
+				id="toggle-switch"
+				className="darkModeInput"
+				onChange={handleToggle}
+			/>
+			<label htmlFor="toggle-switch" className="">
+				{isDarkMode ? (
+					<span className="moon">
+						<span role="img" aria-label="moon" className="moon">
+							🌙
+						</span>
+					</span>
+				) : (
+					<span className="sun">
+						<span role="img" aria-label="sun" className="sun">
+							☀️
+						</span>
+					</span>
+				)}
+			</label>
 		</div>
 	);
 };
