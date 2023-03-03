594f6a2aefbcdea1314859aa6ce3ff9f3b77d073
Thibaut <raimond.thibaut@gmail.com>
2023-03-03 13:11:57 +0100

    Responsive with min-width

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import indexCss from "./styles/index.css"
+import indexCss from "./styles/index.css";
 
 import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// Ajouter une classe à l'élément racine en fonction de l'URL chargée
+if (window.location.pathname.includes("/pokedex/pokemon")) {
+	document.documentElement.classList.add("htmlPokedex");
+}
+
 root.render(
 	<React.StrictMode>
 		<BrowserRouter>
