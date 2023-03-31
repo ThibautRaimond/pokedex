bda0237c3bc4201cb2a60634f0be7aa660300bed
Thibaut <raimond.thibaut@gmail.com>
2023-03-31 14:45:31 +0200

    pokemonLogo better then homeLogo

--- src/components/Layout/Navbar.jsx
+++ src/components/Layout/Navbar.jsx
@@ -1,23 +1,30 @@
 import { Link, useLocation } from "react-router-dom";
 import { AiTwotoneHome } from "react-icons/ai";
-
 import { useTheme } from "../../hooks/useTheme";
 import ThemeToggler from "../Inputs/toggleTheme";
+import LogoPokemon from "../../assets/pokemon.png";
 import "./Navbar.css";
 
 const Navbar = () => {
 	const { theme, toggleTheme } = useTheme();
 	const location = useLocation();
+	const isOnPokedexPage = location.pathname === "/pokedex";
+
+	const handleHomeClick = () => {
+		if (isOnPokedexPage) {
+			window.location.reload();
+		}
+	};
 
 	return (
 		<nav className="navbar">
-			<Link
-				to="/pokedex"
-				onClick={() => {
-					if (location.pathname === "/pokedex") window.location.reload();
-				}}
-			>
-				<AiTwotoneHome className="closeIcon menuIcon" alt="Bouton Accueil"/>
+			<Link to="/pokedex" onClick={handleHomeClick}>
+			<img
+						className="logoPokemon"
+						src={LogoPokemon}
+						alt="Logo Accueil"
+						style={{ width: "75px" }}
+					/>
 			</Link>
 			<h1 className="navbar__title">POKEDEX</h1>
 			<ThemeToggler onChange={toggleTheme} state={theme} icons={["🌙", "☀️"]} />
