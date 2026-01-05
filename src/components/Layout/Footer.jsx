02a8de4fb799b8111729395a75a0facdac8f30d0
Thibaut RAIMOND <raimond.thibaut@gmail.com>
2026-01-05 23:36:00 +0100

    Footer link labels fix

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -8,16 +8,16 @@ const Footer = () => {
 	return (
 		<div className="footerContainer">
 			<div className="footerContainer__socialsMediasContainer">
-				<Link to={`https://github.com/ThibautRaimond`} target="_blank">
-					<AiFillGithub size={45} className="socialsMediasContainer__SocialMedia socialsMediasContainer__Github" alt="Logo Github"/>
+				<Link to={`https://github.com/ThibautRaimond`} target="_blank" aria-label="Github de Thibaut Raimond">
+					<AiFillGithub size={45} className="socialsMediasContainer__SocialMedia socialsMediasContainer__Github" aria-hidden="true"/>
 				</Link>
-				<Link to={`https://www.linkedin.com/in/thibaut-raimond-0a46791ab/`} target="_blank">
-					<AiFillLinkedin size={45} className="socialsMediasContainer__SocialMedia socialsMediasContainer__Linkedin" alt="Logo Linkedin"/>
+				<Link to={`https://www.linkedin.com/in/thibaut-raimond-0a46791ab/`} target="_blank" aria-label="Linkedin de Thibaut Raimond">
+					<AiFillLinkedin size={45} className="socialsMediasContainer__SocialMedia socialsMediasContainer__Linkedin" aria-hidden="true"/>
 				</Link>
 			</div>
 
 			<Link to={`https://thibautraimond.github.io/portfolio/`} target="_blank">
-				<p className="footerContainer__Portfolio">Plus de projets ici sur mon portfolio</p>
+				<p className="footerContainer__Portfolio">Visiter mon portfolio</p>
 			</Link>
 		</div>
 	);
