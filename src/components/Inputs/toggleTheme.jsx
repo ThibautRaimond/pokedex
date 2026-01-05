3e98ee9e28c4a13408a513b3ac4439e73d5ceb81
Thibaut RAIMOND <raimond.thibaut@gmail.com>
2026-01-05 23:20:13 +0100

    fix default label on modeButton

--- src/components/Inputs/toggleTheme.jsx
+++ src/components/Inputs/toggleTheme.jsx
@@ -8,7 +8,10 @@ const ToggleTheme = ({ onChange, state, icons }) => {
 		}
 	};
 
-	const ariaText = state === "dark" ? "Activé le thème clair" : "Activé le thème sombre";
+	// texte pour aria-label du bouton : ce que le clic fera
+	const actionLabel = state === "dark" ? "Activer le thème clair" : "Activer le thème sombre";
+	// texte à vocaliser après changement
+	const liveText = state === "dark" ? "Thème sombre activé" : "Thème clair activé";
 
 	return (
 		<div className="toggleContainer">
@@ -22,6 +25,7 @@ const ToggleTheme = ({ onChange, state, icons }) => {
 				className="toggleLabel"
 				tabIndex="0"
 				role="button"
+				aria-label={actionLabel} // indique ce que le clic fera
 				onClick={onChange}
 				onKeyDown={handleKeyDown}
 			>
@@ -41,7 +45,8 @@ const ToggleTheme = ({ onChange, state, icons }) => {
 					</span>
 				)}
 			</div>
-			{/* élément invisible pour vocaliser le changement */}
+
+			{/* texte invisible pour vocaliser le changement d'état */}
 			<div
 				aria-live="polite"
 				style={{
@@ -51,7 +56,7 @@ const ToggleTheme = ({ onChange, state, icons }) => {
 					overflow: "hidden",
 				}}
 			>
-				{ariaText}
+				{liveText}
 			</div>
 		</div>
 	);
