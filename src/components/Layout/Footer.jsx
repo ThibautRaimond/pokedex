d4def3762340e194a608ee7b9dc205cb1a70f5b9
Thibaut RAIMOND <raimond.thibaut@gmail.com>
2026-01-07 21:17:20 +0100

    list in footer

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -6,20 +6,28 @@ import "./Footer.css"
 
 const Footer = () => {
 	return (
-		<div className="footerContainer">
-			<div className="footerContainer__socialsMediasContainer">
-				<Link to={`https://github.com/ThibautRaimond`} target="_blank" aria-label="Github de Thibaut Raimond">
-					<AiFillGithub size={45} className="socialsMediasContainer__SocialMedia socialsMediasContainer__Github" aria-hidden="true"/>
-				</Link>
-				<Link to={`https://www.linkedin.com/in/thibaut-raimond-0a46791ab/`} target="_blank" aria-label="Linkedin de Thibaut Raimond">
-					<AiFillLinkedin size={45} className="socialsMediasContainer__SocialMedia socialsMediasContainer__Linkedin" aria-hidden="true"/>
-				</Link>
-			</div>
-
-			<Link to={`https://thibautraimond.github.io/portfolio/`} target="_blank">
-				<p className="footerContainer__Portfolio">Visiter mon portfolio</p>
-			</Link>
+		<nav>
+		<ul className="footerContainer">
+		<div className="footerContainer__socialsMediasContainer">
+		<li>
+		<Link to={`https://github.com/ThibautRaimond`} target="_blank" aria-label="Github de Thibaut Raimond">
+		<AiFillGithub size={45} className="socialsMediasContainer__SocialMedia socialsMediasContainer__Github" aria-hidden="true"/>
+		</Link>
+		</li>
+		<li>
+		<Link to={`https://www.linkedin.com/in/thibaut-raimond-0a46791ab/`} target="_blank" aria-label="Linkedin de Thibaut Raimond">
+		<AiFillLinkedin size={45} className="socialsMediasContainer__SocialMedia socialsMediasContainer__Linkedin" aria-hidden="true"/>
+		</Link>
+		</li>
 		</div>
+		
+		<li>
+		<Link to={`https://thibautraimond.github.io/portfolio/`} target="_blank">
+		<p className="footerContainer__Portfolio">Visiter mon portfolio</p>
+		</Link>
+		</li>
+		</ul>
+		</nav>
 	);
 };
 
