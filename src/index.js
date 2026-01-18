a05a5f5950ef669f811b29f6308076ac040596db
Thibaut RAIMOND <raimond.thibaut@gmail.com>
2026-01-18 02:38:57 +0100

    add easterEgg

--- src/index.js
+++ src/index.js
@@ -6,6 +6,20 @@ import { ThemeProvider } from "./hooks/useTheme";
 import App from "./App";
 import "./styles/resetCss.css";
 import "./styles/index.css";
+import cursorMew from "./assets/gam178.cur";
+
+// Fonction globale pour activer le curseur Mew
+window.mew = () => {
+  document.body.classList.add("mew-cursor");
+  console.log("Mew! ✨ Bravo, le curseur a été changé!");
+  console.log("undomew() pour le désactiver.");
+};
+
+// Fonction globale pour désactiver le curseur Mew
+window.undomew = () => {
+  document.body.classList.remove("mew-cursor");
+  console.log("Le curseur est revenu à la normale.");
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
