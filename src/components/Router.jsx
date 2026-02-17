ae177439e9e4e25a5b7f887eb3e32e124d2763b3
Thibaut RAIMOND <raimond.thibaut@gmail.com>
2026-02-17 21:58:14 +0100

    fix: pokemonPageUrl again

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -1,9 +1,14 @@
 import { useEffect } from "react";
-import { Routes, Route, useLocation, Navigate } from "react-router-dom";
+import { Routes, Route, useLocation, Navigate, useParams } from "react-router-dom";
 
 import HomePage from "../pages/HomePage";
 import PokedexPage from "../pages/PokedexPage";
 import NotFoundPage from "../pages/NotFoundPage";
+
+const PokemonRedirect = () => {
+  const { id } = useParams();
+  return <Navigate to={`/pokemon/${id}`} replace />;
+};
 
 const Routeur = () => {
   const location = useLocation();
@@ -31,6 +36,8 @@ const Routeur = () => {
 
   return (
     <Routes location={location.pathname}>
+      <Route path="/pokedex" element={<Navigate to="/" replace />} />
+      <Route path="/pokedex/pokemon/:id" element={<PokemonRedirect />} />
       <Route path="/" element={<HomePage />} />
       <Route path="/pokemon/:id" element={<PokedexPage />} />
       <Route path="*" element={<NotFoundPage />} />
